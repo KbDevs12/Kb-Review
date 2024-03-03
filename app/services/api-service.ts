277bd98dd10949046cb85425009bd45c6682816f
KbDevs12 <161736782+KbDevs12@users.noreply.github.com>
2024-03-03 23:36:18 +0700

    Update api-service.ts

--- app/services/api-service.ts
+++ app/services/api-service.ts
@@ -1,5 +1,5 @@
 export const getAnimeResponse = async (resource: string, query: string) => {
-  const response = await fetch(`${process.env.JIKAN_API}/${resource}/${query}`);
+  const response = await fetch(`${process.env.JIKAN_API}/${resource}?${query}`);
   const anime = await response.json();
   return anime;
 };
