b1224dd8f1389b48564d2e58e7c81ff59b9da968
KbDevs12 <161736782+KbDevs12@users.noreply.github.com>
2024-03-05 10:48:17 +0700

    Update page.tsx

--- app/search/[keyword]/page.tsx
+++ app/search/[keyword]/page.tsx
@@ -7,20 +7,6 @@ const Page = async ({ params }: { params: any }) => {
   const titles = decodeURIComponent(params.keyword);
 
   const Hasil = await getAnimeResponse("anime", `q=${key}`);
-
-  if (!Hasil || !Hasil.results || Hasil.results.length === 0) {
-    // Jika hasil pencarian kosong, tampilkan pesan
-    return (
-      <>
-        <Header title={`Hasil Pencarian "${titles}":`} />
-        <p className="text-center text-md md:text-2xl">
-          Tidak ada anime bernama {titles}
-        </p>
-      </>
-    );
-  }
-
-  // Jika ada hasil pencarian, tampilkan list anime
   return (
     <>
       <Header title={`Hasil Pencarian ${titles} :`} />
