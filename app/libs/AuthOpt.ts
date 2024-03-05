81b7976ca128185214a1e4849c0b53a2ef60c91d
KbDevs12 <161736782+KbDevs12@users.noreply.github.com>
2024-03-05 11:05:16 +0700

    Create AuthOpt.ts

---
+++ app/libs/AuthOpt.ts
@@ -0,0 +1,10 @@
+import githubAuth from "next-auth/providers/github";
+export const authOpt = {
+  providers: [
+    githubAuth({
+      clientId: `${process.env.GITHUB_CLIENT}`,
+      clientSecret: `${process.env.GITHUB_SECRET}`,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
